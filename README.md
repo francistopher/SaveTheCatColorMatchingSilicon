7d42c18798bc27b833151063780f586a1c1333e9
francistopher <francistopherrr@outlook.com>
2022-09-19 12:53:19 -0400

    CREATE Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Save The Cat Color Matching
+
+- A rewrite of the codebase written on an Intel powered Mac
+- Source code execution is compatible with Apple Sillicon powered Macs
+
+## Compatibility
+
+| OS | Compatible |
+| -- | ---------- |
+| iOS | :x: |
+| PadOS | :x: |
+| MacOS | :x: |
+| WatchOS | :x: |
+
---
+++ SaveTheCatColorMatchingSilicon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheCatColorMatchingSilicon
+//
+//  Created by Christopher Francisco on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
